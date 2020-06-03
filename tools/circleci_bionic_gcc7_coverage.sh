0787be0ab73a703c439a60b6cd24b46aca96f317
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-03 23:50:08 +0200

    feat(circleci) Initial setup.

---
+++ tools/circleci_bionic_gcc7_coverage.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+#sudo apt-get install build-essential cmake libboost-dev libboost-serialization-dev libboost-test-dev libnlopt-dev libeigen3-dev coinor-libipopt-dev curl libtbb-dev
+sudo apt-get install build-essential cmake
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+# GCC build with coverage.
+cmake ../ -DCMAKE_BUILD_TYPE=Debug -DPAGMO_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="--coverage"
+make -j2 VERBOSE=1
+ctest -V
+
+# Upload coverage data.
+bash <(curl -s https://codecov.io/bash) -x gcov-7
+
+set +e
+set +x
