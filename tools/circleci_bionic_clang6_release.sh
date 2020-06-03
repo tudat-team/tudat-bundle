9a694d5265785fdf30554cfd56cf74d47ac326b6
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-04 01:57:00 +0200

    feat(circleci) Attempting macosx xcode 11 build on circleci.

---
+++ tools/circleci_bionic_clang6_release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install build-essential cmake
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+# Clang release build.
+CXX=clang++ CC=clang cmake ../ -DCMAKE_BUILD_TYPE=Release
+make -j2 VERBOSE=1
+ctest -V
+
+set +e
+set +x
