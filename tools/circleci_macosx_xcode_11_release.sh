a3ae71462e1c1b25237b2795bddf5193e42e74ff
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-04 01:51:47 +0200

    feat(circleci) Attempting macosx xcode 11 build on circleci.

--- tools/circleci_macosx_xcode_11_release.sh
+++ tools/circleci_macosx_xcode_11_release.sh
@@ -14,12 +14,9 @@ mkdir build
 cd build
 
 # GCC build with coverage.
-cmake ../ -DCMAKE_BUILD_TYPE=Debug -DPAGMO_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="--coverage"
+cmake ../ -DCMAKE_BUILD_TYPE=Release
 make -j2 VERBOSE=1
 ctest -V
-
-# Upload coverage data.
-bash <(curl -s https://codecov.io/bash) -x gcov-7
 
 set +e
 set +x
